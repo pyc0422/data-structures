c85107b00b959babd65116edc5e3febd984d3250
viviennema <98420335+viviennema@users.noreply.github.com>
2022-06-07 15:00:56 -0700

    finished functional part

--- sprint-one/src/functional/queue.js
+++ sprint-one/src/functional/queue.js
@@ -3,16 +3,28 @@ var Queue = function() {
 
   // Use an object with numeric keys to store values
   var storage = {};
+  var start = 0;
+  var end = 0;
 
   // Implement the methods below
 
   someInstance.enqueue = function(value) {
+    someInstance[end] = value;
+    end++;
   };
 
   someInstance.dequeue = function() {
+    if (start < end) {
+      var value = someInstance[start];
+      delete someInstance[start];
+      start++;
+      return value;
+    }
+
   };
 
   someInstance.size = function() {
+    return end - start;
   };
 
   return someInstance;
