caca74ae4ee90c312d0be31a7d23974f5b84689d
Rain <pyc0422@gmail.com>
2022-06-07 22:50:43 -0500

    "Complete prototypal stack and queue"

--- sprint-one/src/prototypal/stack.js
+++ sprint-one/src/prototypal/stack.js
@@ -1,8 +1,27 @@
 var Stack = function() {
-  // Hey! Rewrite in the new style. Your code will wind up looking very similar,
-  // but try not not reference your old code in writing the new style.
+  var someInstance = Object.create(stackMethods);
+  return someInstance;
 };
 
-var stackMethods = {};
+var stackMethods = {
+  length: 0,
+  push: function(val) {
+    this[this.length] = val;
+    this.length++;
+    return this.length;
+  },
+  pop: function() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    var delValue = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length --;
+    return delValue;
+  },
+  size: function() {
+    return this.length;
+  }
+};
 
 
