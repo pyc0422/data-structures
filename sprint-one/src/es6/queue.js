811bb51823a70f47ea69a41fc2d4b871a72b044b
Rain <pyc0422@gmail.com>
2022-06-07 23:36:05 -0500

    "Complete sprint one"

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,37 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
+  }
+
+  enqueue(val) {
+    var key = this.length;
+    while (this[key]) {
+      key ++;
+    }
+    this[key] = val;
+    this.length ++;
+    return this.length;
+  }
+
+  dequeue() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    var curKey = 0;
+    while (!this[curKey]) {
+      curKey ++;
+    }
+    var first = this[curKey];
+    delete this[curKey];
+    this.length --;
+    return first;
+  }
+
+  size() {
+    return this.length;
   }
 
 }
+
+var newQueue = new Queue();
--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,29 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.length = 0;
   }
 
+  push(val) {
+    this[this.length] = val;
+    this.length ++;
+    return this.length;
+  }
+
+  pop() {
+    if (this.length === 0) {
+      return undefined;
+    }
+    var del = this[this.length - 1];
+    delete this[this.length - 1];
+    this.length --;
+    return del;
+  }
+
+  size() {
+    return this.length;
+  }
 }
+
+var newStack = new Stack();
+
