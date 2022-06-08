dd446be64b6ca4700b2f032db2c786b2b4976131
Rain <pyc0422@gmail.com>
2022-06-07 23:03:50 -0500

    "Complete pseudoclassical stack and queue"

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,34 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
+
+Queue.prototype.enqueue = function(val) {
+  var key = this.length;
+  while (this[key]) {
+    key ++;
+  }
+  this[key] = val;
+  this.length ++;
+  return this.length;
+};
+Queue.prototype.dequeue = function() {
+  if (this.length === 0) {
+    return undefined;
+  }
+  var curKey = 0;
+  while (!this[curKey]) {
+    curKey ++;
+  }
+  var first = this[curKey];
+  delete this[curKey];
+  this.length --;
+  return first;
+};
+Queue.prototype.size = function() {
+  return this.length;
+};
+var newQueue = new Queue();
 
 
--- sprint-one/src/pseudoclassical/stack.js
+++ sprint-one/src/pseudoclassical/stack.js
@@ -1,6 +1,24 @@
 var Stack = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.length = 0;
 };
 
-
+Stack.prototype.push = function(val) {
+  this[this.length] = val;
+  this.length ++;
+  return this.length;
+};
+Stack.prototype.pop = function() {
+  if (this.length === 0) {
+    return undefined;
+  }
+  var delVal = this[this.length - 1];
+  delete this[this.length - 1];
+  this.length --;
+  return delVal;
+};
+Stack.prototype.size = function() {
+  return this.length;
+};
+var stack = new Stack();
