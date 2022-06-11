fd1bc7ed6df1bbe0c6bb22eb6f0681cc67d861e1
viviennema <98420335+viviennema@users.noreply.github.com>
2022-06-11 12:14:57 -0700

    finished tree

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,62 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  newTree.children = []; // fix me
+  _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // {value: 0, children: [
+  //  {value:1, children: [
+  //
+  //  ]},
+  //  {value: 2, chinldren:[{subavalue1:4, children:[{}]}]
+
+  // }
+  //]}
+  var newTree = Tree(value);
+  this.children.push(newTree);
+
 };
 
 treeMethods.contains = function(target) {
+  //  {
+  //   value: 2,
+  //   chinldren:[
+  //        {
+  //          subavalue1:4,
+  //          children:[
+  //            {
+
+  //             }]
+  //            }]
+  //   }
+  // base case: if list.value === tagert return true;
+  // recursion name(checkList): if not children.length === 0, return false;
+  // else: for (var i = 0) checkList(list.children[i]);
+
+
+  var checkList = function(list) {
+    if (list.value === target) {
+      return true;
+    }
+    if (list.children.length === 0) {
+      return false;
+    }
+    for (var i = 0; i < list.children.length; i++) {
+      var res = checkList(list.children[i]);
+      if (res === true) {
+        return true;
+      }
+    }
+    return false;
+
+  };
+  return checkList(this);
+
 };
 
 
