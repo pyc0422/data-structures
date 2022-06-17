10ff1d94e38a752fc85ba7715a8492cddbb43256
viviennema <98420335+viviennema@users.noreply.github.com>
2022-06-17 11:19:39 -0700

    Complete hashTable

--- sprint-two/src/graph.js
+++ sprint-two/src/graph.js
@@ -70,6 +70,13 @@ Graph.prototype.forEachNode = function(cb) {
 var graph = new Graph();
 /*
  * Complexity: What is the time complexity of the above functions?
+ addNode: O(1);
+ contains: O(1);
+ remove:O(1);
+ hasEdge: O(n);
+ addEdge: O(1);
+ removeEdge: O(n);
+ forEach: O(n);
  */
 
 
